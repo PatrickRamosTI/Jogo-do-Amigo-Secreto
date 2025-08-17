b7c57df900e9282e884fb59d362332d41fa3bebe
Patrick Ramos <patrickramosTi@gmail.com>
2025-08-17 20:05:10 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+let amigos = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    amigos.push(nome);
+    input.value = "";
+    exibirAmigos();
+}
+
+function exibirAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    let resultado = document.getElementById("resultado");
+
+    if (amigos.length === 0) {
+        alert("Adicione pelo menos um nome antes de sortear.");
+        return;
+    }
+
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceSorteado];
+
+    resultado.innerHTML = `<li> O amigo secreto sorteado foi: <strong>${amigoSorteado}</strong></li>`;
+}
